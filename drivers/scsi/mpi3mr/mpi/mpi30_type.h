df871c7c84c1d29ce6265fed0724ca49e27645af
Kashyap Desai <kashyap.desai@broadcom.com>
2021-04-16 17:48:09 +0530

    mpi3mr: add mpi30 Rev-R headers and Kconfig
    This adds the Kconfig and mpi30 headers.
    
    Signed-off-by: Kashyap Desai <kashyap.desai@broadcom.com>
    
    Cc: sathya.prakash@broadcom.com
    Cc: bvanassche@acm.org
    Cc: thenzl@redhat.com
    Cc: hare@suse.de

---
+++ drivers/scsi/mpi3mr/mpi/mpi30_type.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ *  Copyright 2016-2020 Broadcom Inc. All rights reserved.
+ *
+ *           Name: mpi30_type.h
+ *    Description: MPI basic type definitions
+ *  Creation Date: 10/07/2016
+ *        Version: 03.00.00
+ */
+#ifndef MPI30_TYPE_H
+#define MPI30_TYPE_H     1
+
+#define MPI3_POINTER    *
+
+typedef u8 U8;
+typedef __le16 U16;
+typedef __le32 U32;
+typedef __le64 U64 __aligned(4);
+
+#endif  /* MPI30_TYPE_H */
